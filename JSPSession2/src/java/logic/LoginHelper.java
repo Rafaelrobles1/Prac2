b2eb76f58f632b39bd41ec65bd839ce9f77e8c58
salvador202 <kairos_dark04@ciencias.unam.mx>
2016-04-05 18:10:17 -0500

    Practica completa

---
+++ JSPSession2/src/java/logic/LoginHelper.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import model.Login;
+
+/**
+ *
+ * @author miguel
+ */
+public class LoginHelper {
+
+    private Session session;
+
+    public LoginHelper() {
+        try{
+        
+            session = HibernateUtil.getSessionFactory().openSession();
+        }
+        catch(Exception EX){
+                session = HibernateUtil.getSessionFactory().getCurrentSession();
+        }
+    }
+
+    public Login getLoginPorNombre(String nombre) {
+        Login usuario;
+        try {
+            Transaction tx = session.beginTransaction();
+            Query q = session.getNamedQuery("BuscaPorNombre").setString("usuario", nombre);
+            return (Login) q.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
